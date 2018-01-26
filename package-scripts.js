639c410f45e9a361417acba72586fe6e4d294fe5
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-01-26 00:11:16 -0800

    chore: ran generator

--- package-scripts.js
+++ package-scripts.js
@@ -1,16 +1,19 @@
 const {
   concurrent,
   crossEnv,
+  ifNotWindows,
+  ifWindows,
   mkdirp,
   series,
-  ifWindows,
-  ifNotWindows,
+  setColors,
 } = require('nps-utils')
 const pjson = require('./package.json')
 const release = pjson.devDependencies.typedoc ? ['ci.release.semantic-release', 'ci.release.typedoc'] : ['ci.release.semantic-release']
 const script = (script, description) => description ? {script, description} : {script}
 const hidden = script => ({script, hiddenFromHelp: true})
 const unixOrWindows = (unix, windows) => series(ifNotWindows(unix), ifWindows(windows))
+
+setColors(['dim'])
 
 let ciTests = [
   'ci.test.eslint',
@@ -33,10 +36,13 @@ module.exports = {
       series: script(series.nps('lint', 'test.mocha'), 'lint and run all tests in series'),
       mocha: {
         default: script('mocha --forbid-only "test/**/*.test.ts"', 'run all mocha tests'),
-        coverage: hidden(series.nps('test.mocha.nyc nps test.mocha')),
+        coverage: {
+          default: hidden(series.nps('test.mocha.nyc nps test.mocha', 'test.mocha.coverage.report')),
+          report: hidden(series('nps "test.mocha.nyc report --reporter text-lcov" > coverage.lcov')),
+        },
         junit: hidden(series(
           crossEnv('MOCHA_FILE="reports/mocha.xml" ') + series.nps('test.mocha.nyc nps \\"test.mocha --reporter mocha-junit-reporter\\"'),
-          series.nps('test.mocha.nyc report text-lcov > coverage.lcov'),
+          series.nps('test.mocha.coverage.report'),
         )),
         nyc: hidden('nyc --nycrc-path node_modules/@dxcli/dev-nyc-config/.nycrc'),
       },
@@ -53,7 +59,7 @@ module.exports = {
         mocha: hidden(
           unixOrWindows(
             series.nps('test.mocha.junit'),
-            series.nps('test.mocha'),
+            series.nps('test.mocha.coverage'),
           )
         ),
         eslint: hidden(
@@ -69,12 +75,13 @@ module.exports = {
           )
         ),
       },
+      typedoc: hidden('typedoc --out /tmp/docs src/index.ts --excludeNotExported --mode file'),
       release: {
         default: hidden(series.nps(...release)),
         'semantic-release': hidden('semantic-release -e @dxcli/dev-semantic-release'),
         typedoc: hidden(series(
           'git clone -b gh-pages $CIRCLE_REPOSITORY_URL gh-pages',
-          'typedoc --out /tmp/docs src/index.ts --excludeNotExported --mode file',
+          'nps ci.typedoc',
           'rm -rf ./gh-pages/*',
           'mv /tmp/docs/* ./gh-pages',
           'cd gh-pages && git add . && git commit -m "updates from $CIRCLE_SHA1 [skip ci]" && git push',
