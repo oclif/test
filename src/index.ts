006c90c33c1a6891148189227bdc27a0a271dc6f
Mike Donnalley <mdonnalley@salesforce.com>
2024-05-15 10:39:29 -0600

    feat: new oclif test utilities

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,78 @@
-import * as fancyTest from 'fancy-test'
+import {Config, Errors, Interfaces, run} from '@oclif/core'
+import ansis from 'ansis'
+import makeDebug from 'debug'
 import {dirname} from 'node:path'
 
-import {command} from './command'
-import exit from './exit'
-import hook from './hook'
-import {loadConfig} from './load-config'
+const debug = makeDebug('test')
+
+type CaptureOptions = {
+  print?: boolean
+  stripAnsi?: boolean
+}
+
+const RECORD_OPTIONS: CaptureOptions = {
+  print: false,
+  stripAnsi: true,
+}
+
+const originals = {
+  stderr: process.stderr.write,
+  stdout: process.stdout.write,
+}
+
+const output: Record<'stderr' | 'stdout', Array<Uint8Array | string>> = {
+  stderr: [],
+  stdout: [],
+}
+
+function mockedStdout(str: Uint8Array | string, cb?: (err?: Error) => void): boolean
+function mockedStdout(str: Uint8Array | string, encoding?: BufferEncoding, cb?: (err?: Error) => void): boolean
+function mockedStdout(
+  str: Uint8Array | string,
+  encoding?: ((err?: Error) => void) | BufferEncoding,
+  cb?: (err?: Error) => void,
+): boolean {
+  output.stdout.push(str)
+  if (!RECORD_OPTIONS.print) return true
+
+  if (typeof encoding === 'string') {
+    return originals.stdout.bind(process.stdout)(str, encoding, cb)
+  }
+
+  return originals.stdout.bind(process.stdout)(str, cb)
+}
+
+function mockedStderr(str: Uint8Array | string, cb?: (err?: Error) => void): boolean
+function mockedStderr(str: Uint8Array | string, encoding?: BufferEncoding, cb?: (err?: Error) => void): boolean
+function mockedStderr(
+  str: Uint8Array | string,
+  encoding?: ((err?: Error) => void) | BufferEncoding,
+  cb?: (err?: Error) => void,
+): boolean {
+  output.stderr.push(str)
+  if (!RECORD_OPTIONS.print) return true
+  if (typeof encoding === 'string') {
+    return originals.stdout.bind(process.stderr)(str, encoding, cb)
+  }
+
+  return originals.stdout.bind(process.stderr)(str, cb)
+}
+
+const restore = (): void => {
+  process.stderr.write = originals.stderr
+  process.stdout.write = originals.stdout
+}
+
+const reset = (): void => {
+  output.stderr = []
+  output.stdout = []
+}
+
+const toString = (str: Uint8Array | string): string =>
+  RECORD_OPTIONS.stripAnsi ? ansis.strip(str.toString()) : str.toString()
+
+const getStderr = (): string => output.stderr.map((b) => toString(b)).join('')
+const getStdout = (): string => output.stdout.map((b) => toString(b)).join('')
 
 function traverseFilePathUntil(filename: string, predicate: (filename: string) => boolean): string {
   let current = filename
@@ -15,27 +83,84 @@ function traverseFilePathUntil(filename: string, predicate: (filename: string) =
   return current
 }
 
-/* eslint-disable unicorn/prefer-module */
-loadConfig.root =
-  process.env.OCLIF_TEST_ROOT ??
-  Object.values(require.cache).find((m) => m?.children.includes(module))?.filename ??
-  traverseFilePathUntil(
-    require.main?.path ?? module.path,
-    (p) => !(p.includes('node_modules') || p.includes('.pnpm') || p.includes('.yarn')),
+function makeLoadOptions(loadOpts?: Interfaces.LoadOptions): Interfaces.LoadOptions {
+  return (
+    loadOpts ?? {
+      root: traverseFilePathUntil(
+        // eslint-disable-next-line unicorn/prefer-module
+        require.main?.path ?? module.path,
+        (p) => !(p.includes('node_modules') || p.includes('.pnpm') || p.includes('.yarn')),
+      ),
+    }
   )
-/* eslint-enable unicorn/prefer-module */
+}
 
-// Using a named export to import fancy causes this issue: https://github.com/oclif/test/issues/516
-export const test = fancyTest.fancy
-  .register('loadConfig', loadConfig)
-  .register('command', command)
-  .register('exit', exit)
-  .register('hook', hook)
-  .env({NODE_ENV: 'test'})
+export async function captureOutput<T>(
+  fn: () => Promise<unknown>,
+  opts?: CaptureOptions,
+): Promise<{
+  error?: Error & Partial<Errors.CLIError>
+  result?: T
+  stderr: string
+  stdout: string
+}> {
+  RECORD_OPTIONS.print = opts?.print ?? false
+  RECORD_OPTIONS.stripAnsi = opts?.stripAnsi ?? true
+  process.stderr.write = mockedStderr
+  process.stdout.write = mockedStdout
 
-export default test
+  try {
+    const result = await fn()
+    return {
+      result: result as T,
+      stderr: getStderr(),
+      stdout: getStdout(),
+    }
+  } catch (error) {
+    return {
+      ...(error instanceof Errors.CLIError && {error}),
+      ...(error instanceof Error && {error}),
+      stderr: getStderr(),
+      stdout: getStdout(),
+    }
+  } finally {
+    restore()
+    reset()
+  }
+}
 
-export {command} from './command'
+export async function runCommand<T>(
+  args: string[],
+  loadOpts?: Interfaces.LoadOptions,
+  captureOpts?: CaptureOptions,
+): Promise<{
+  error?: Error & Partial<Errors.CLIError>
+  result?: T
+  stderr: string
+  stdout: string
+}> {
+  const loadOptions = makeLoadOptions(loadOpts)
+  debug('loadOpts: %O', loadOpts)
+  return captureOutput<T>(async () => run(args, loadOptions), captureOpts)
+}
 
-export {Config} from '@oclif/core'
-export {FancyTypes, expect} from 'fancy-test'
+export async function runHook<T>(
+  hook: string,
+  options: Record<string, unknown>,
+  loadOpts?: Interfaces.LoadOptions,
+  recordOpts?: CaptureOptions,
+): Promise<{
+  error?: Error & Partial<Errors.CLIError>
+  result?: T
+  stderr: string
+  stdout: string
+}> {
+  const loadOptions = makeLoadOptions(loadOpts)
+
+  debug('loadOpts: %O', loadOpts)
+
+  return captureOutput<T>(async () => {
+    const config = await Config.load(loadOptions)
+    return config.runHook(hook, options)
+  }, recordOpts)
+}
